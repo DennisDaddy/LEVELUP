f4c2b4e0f2ef1acb4dc54a30f64eeffa400ade38
DennisDaddy <softteez95@gmail.com>
2018-06-27 10:04:19 +0300

    Create User class, register and login class

---
+++ user.py
@@ -0,0 +1,12 @@
+class User(object):
+	"""docstring for User"""
+	def __init__(self, arg):
+		super(User, self).__init__()
+		self.arg = arg
+	
+	def register():
+			pass	
+
+	
+	def login():
+			pass		
