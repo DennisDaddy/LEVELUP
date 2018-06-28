5049cd69cdaf14f64f568856b9fb05b151f98410
DennisDaddy <softteez95@gmail.com>
2018-06-28 21:16:59 +0300

    add other user signup inputs

--- user_oop.py
+++ user_oop.py
@@ -1,10 +1,10 @@
-import sessions
 
 class User(object):
 	
 
 	def __init__(self):
 		self.users = {}
+		self.comment = {}
 		
 
 		option = input("Press Yes if Registered or No if you have an account to login ? Yes/No? ")
@@ -17,13 +17,16 @@ class User(object):
 	
 	def register(self):
 		user_enter = input("Enter user name: ")
+		first_name = input("Enter first name: ")
+		last_name = input("Enter last name: ")
+		email = input("Enter your email: ")
 
 		if user_enter in self.users:
-			print("\nLogin name already exists!\n")
+			print("\nUser name already exists!\n")
 		else:
 		    createPassw = input("Create new password: ")
 		    self.users[user_enter] = createPassw
-		    print("\n User successfully created\n")	
+		    print("\n User successfully created enter your details below to login\n")	
 
 	def log_user(self):
 		login = input("Enter user name: ")
@@ -32,10 +35,14 @@ class User(object):
 		if login in self.users and self.users[login] == passw:
 			print("\nLogin successfully!\n")
 		else:
-		    print("\nUser doesn't exists or wrong password!\n")
-	def comment():
-		    	pass	    
+		    print("\nUser doesn't exists or you have entered wrong password!\n")
+
+	def add_comment(self,content):
+		self.comment[content] = content
+
+		return {"info": "content successfully added"}	    
 
 dennis =User()
 dennis.register()
 dennis.log_user()
+dennis.add_comment()
