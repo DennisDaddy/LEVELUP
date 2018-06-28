adf0c79d733dce87dcf24623f8ec167e7e7d02cd
DennisDaddy <softteez95@gmail.com>
2018-06-28 15:29:09 +0300

    Create user class

---
+++ user_oop.py
@@ -0,0 +1,12 @@
+class User(object):
+	
+	def __init__(self):
+		self.user_info = {}
+		self.users = users
+		
+	def welcome_message():
+			pass	
+
+	def register(self, username, email, password, password_confirmation):
+		self.user_info['username'] = username
+		self.user_info['email'] = email
