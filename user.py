fdc809c3c6082b6a25fdfa5c8aa3a8b46b1c7831
DennisDaddy <softteez95@gmail.com>
2018-06-28 07:56:58 +0300

    Replace raw_input with input

--- user.py
+++ user.py
@@ -1,32 +1,25 @@
 class User(object):
 	"""docstring for User"""
-	def __init__(self, first_name, last_name, password):
-		self.first_name = first_name
-		self.last_name = last_name
-		self.password = password
-		self.full_name = first_name + " " + last_name
-
-class Register(User):
-	def sign_up(self):
-		first_name = raw_input("Enter first_name :")
-		last_name = raw_input("Enter Last Name : ")
-		email = raw_input("Enter Email: ")
-		password = raw_input("Enter password: ")
-		password_confirmation = raw_input("Confirm password: ")
+	def register(self):
+		print ("Welcome to commandline App enter your details below to Register")
+		first_name = input("Enter your first name :")
+		last_name = input("Enter your Last Name : ")
+		user_name = input("Enter your user Name : ")
+		email = input("Enter your Email: ")
+		password = input("Enter your password: ")
+		password_confirmation = input("Confirm password: ")
 		fullname = first_name + " " + last_name
 
 		print("you are %s and you are successfully registered " %fullname)
-class Login(User):
-	def log_in(self):
+
+	def login(self):
 		print ("Enter your details below to Login.")
-		name = raw_input("Enter your Name : ")
-		email = raw_input("Enter Email: ")	
-		print("you are %s and you are successfully logged in " %name)
+		user_name = input("Enter your user Name : ")
+		email = input("Enter Email: ")	
+		print("you are %s and you are successfully logged in " %user_name)
 
 
+deno =User()
+deno.register()
+deno.login()
 
-deno =Register("one", "two", "three")
-deno.sign_up()
-
-deno =Login("one", "two", "three")
-deno.log_in()
