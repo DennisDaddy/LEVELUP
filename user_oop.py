fabc21b8100fda650d6f72a69c39779c3eae36ff
DennisDaddy <softteez95@gmail.com>
2018-06-28 18:12:23 +0300

    Add a method to login and register user

--- user_oop.py
+++ user_oop.py
@@ -1,12 +1,38 @@
 class User(object):
 	
-	def __init__(self):
-		self.user_info = {}
-		self.users = users
-		
-	def welcome_message():
-			pass	
 
-	def register(self, username, email, password, password_confirmation):
-		self.user_info['username'] = username
-		self.user_info['email'] = email
+	def __init__(self):
+		self.users = {}
+		
+
+		option = input("Press Yes if Registered or No if you have an account to login ? Yes/No? ")
+
+		if option == "Yes":
+			self.log_user()
+
+		elif option == "No":
+		    self.register()
+	
+	def register(self):
+		user_enter = input("Enter user name: ")
+
+		if user_enter in self.users:
+			print("\nLogin name already exists!\n")
+		else:
+		    createPassw = input("Create new password: ")
+		    self.users[user_enter] = createPassw
+		    print("\n User successfully created\n")	
+
+	def log_user(self):
+		login = input("Enter user name: ")
+		passw = input("Enter password: ")
+
+		if login in self.users and self.users[login] == passw:
+			print("\nLogin successfully!\n")
+		else:
+		    print("\nUser doesn't exists or wrong password!\n")
+
+
+dennis =User()
+dennis.register()
+dennis.log_user()
