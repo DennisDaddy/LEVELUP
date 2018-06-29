0277d170c8f9cb286d45fca4ba555906ff89dcab
DennisDaddy <softteez95@gmail.com>
2018-06-29 17:29:44 +0300

    Add functions

--- Flask/user.py
+++ Flask/user.py
@@ -4,6 +4,25 @@ app = Flask(__name__)
 
 @app.route('/')
 
-def hello_world():
-	return 'Hello world'
+def home():
+	return 'Welcom to Flask levelUp Application'
 
+@app.route('/register')
+
+def register():
+	return 'Register here'
+
+@app.route('/login')
+def login():
+	return 'Login here'
+
+@app.route('/posts/new')
+def add_post():
+	return 'Add new post'
+
+@app.route('/comments/new')
+def add_comment():
+	return 'Add new comment here'		
+
+if __name__ == '__main__':
+	app.run(debug=True)
