e5553ba74a0c39e80cfb8da874dc4428ab2aede8
DennisDaddy <softteez95@gmail.com>
2018-06-30 10:02:16 +0300

    Enable user login

--- Flask/user.py
+++ Flask/user.py
@@ -1,20 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, session, request
 
 app = Flask(__name__)
 
-@app.route('/')
+users = {}
 
+@app.route('/')
 def home():
 	return 'Welcom to Flask levelUp Application'
 
-@app.route('/register')
-
+@app.route('/register', methods=['GET', 'POST'])
 def register():
+	if request.method == 'POST':
+		if request.form['username']:
+
+			return render_template('register.html')
 	return render_template('register.html')
 
-@app.route('/login', methods=['GET'], ['POST'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-	return render_template('login.html')
+	error = None
+	if request.method == 'POST':
+		if valid_login(request.form['username'],
+			request.form['password']):
+		    return render_template('login.html')
+	return render_template('login.html', error=error)
 
 @app.route('/posts/new')
 def add_post():
@@ -22,7 +31,11 @@ def add_post():
 
 @app.route('/comments/new')
 def add_comment():
-	return 'Add new comment here'		
+	return 'Add new comment here'	
+
+@app.route('/logout')
+def logout():
+	pass
 
 if __name__ == '__main__':
 	app.run(debug=True)
