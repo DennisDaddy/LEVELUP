081e282d0844bab7a8de7b69c7d4c2f23f9a0ba3
DennisDaddy <softteez95@gmail.com>
2018-07-02 04:27:23 +0300

    Test that flask was set up correctly

--- Flask/test_user.py
+++ Flask/test_user.py
@@ -1,8 +1,14 @@
+from user import app
 import unittest 
 
 class FlaskTestCase(unittest.TestCase):
+	
+	# Test that flask was set up correctly
 	def test_index(self):
-		pass
+		tester = app.test_client(self)
+		response =tester.get('/login', content_type='html/text')
+		self.assertEqual(response.status_code, 200)
+
 
 if __name__ == '__main__':
 			unittest.main()		
