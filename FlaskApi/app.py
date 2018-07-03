249eb33fee7f4743f1c17e01b11e6da494889fc7
DennisDaddy <softteez95@gmail.com>
2018-07-03 09:58:48 +0300

    remove access token error

--- FlaskApi/app.py
+++ FlaskApi/app.py
@@ -3,53 +3,48 @@ import requests
 from requests.auth import HTTPBasicAuth
 import time
 import base64
-
-
+import json
 app = Flask(__name__)
 
-@app.route('/mpesa/api', methods= ['POST'])
+@app.route('/mpesa', methods=['POST'])
 def api_message():
-	data = request.data
-	print(data)
-	return "already run"
+	pass
 
-timestamp = str(time.strftime("%Y%m%d%H%M%S"))
-
-password = 
-
-consumer_key = ""
-consumer_secret = ""
-api_URL = ""
-
-r = requests.get(api_URL, auth=HTTPBasicAuth(consumer_key,consumer_secret))
-
-print(r.text)
+response1 = requests.get("https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials",auth=('3glGFjDvn6RZz9qyo02iA5HXKsImulew','MCxBBj6cdIpGYfbq')).text
+res=json.loads(response1)
+access_token = res['access_token']
+api_url = "https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest"
 
 
-access_token = "{}".format(r.text)
-api_url = ""
 
-headers = { }
+amount = int(input('enter amount'))
+
+phone = input("enter your number")
+
+access_token = res['access_token']
+api_url = "http://sandbox.safaricom.co.ke/mpesa/c2b/v1/registerurl"
+
+
+headers = { "Authorization": "Bearer "+access_token}
 
 request = {
-      "BusinessShortCode": "601754",
-      "Password": "{}".format(password),
-      "Timestamp": "{}".format(timestamp),
+      "BusinessShortCode": "174379",
+      "Password": "MTc0Mzc5YmZiMjc5ZjlhYTliZGJjZjE1OGU5N2RkNzFhNDY3Y2QyZTBjODkzMDU5YjEwZjc4ZTZiNzJhZGExZWQyYzkxOTIwMTgwNzAzMDc0MjAw",
+      "Timestamp": "20180703074200",
       "TransactionType": "CustomerPayBillOnline",
-      "Amount": "100",
-      "PartyA": "254799989486",
-      "PartyB": "601754",
-      "PhoneNumber": "254799989486",
-      "CallBackURL": "https://dennis.ml/callback.html",
-      "AccountReference": "account",
-      "TransactionDesc": "test"
-}
+      "Amount": amount,
+      "PartyA": "254"+phone,
+      "PartyB": "174379",
+      "PhoneNumber": "254706480612",
+      "CallBackURL": "http://mpesa-requestbin.herokuapp.com/1i6fj7k1",
+      "AccountReference": "Dennis Wedding ",
+      "TransactionDesc": "blue"
+    }
+
 
 response = requests.post(api_url, json = request, headers=headers)
 
-print(response.text)
-
-
+print (response.text)	
 
 if __name__ == '__main__':
-		app.run(debug=True)	
+	app.run(debug=True)
