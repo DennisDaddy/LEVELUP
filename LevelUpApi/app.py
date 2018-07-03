3d0b1827da6804563f8221f5476837d0cab541f3
DennisDaddy <softteez95@gmail.com>
2018-07-03 11:52:56 +0300

    Add routes

--- LevelUpApi/app.py
+++ LevelUpApi/app.py
@@ -1,10 +1,19 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app= Flask(__name__)
 
 @app.route('/')
 def index():
 	return "hello world"
+
+@app.route('/level/api/login', methods=['GET', 'POST'])
+def login():
+	pass
+
+
+@app.route('/level/api/comments', methods=['GET'])
+def comments():
+	return jsonify({'comments': comments})
 
 if __name__ == '__main__':
 		app.run(debug=True)	
