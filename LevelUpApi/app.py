17ba0485dac4a6886185973fe0717e5f37245aaf
DennisDaddy <softteez95@gmail.com>
2018-07-06 11:14:41 +0300

    Remove unnecessary indentations

--- LevelUpApi/app.py
+++ LevelUpApi/app.py
@@ -1,16 +1,15 @@
 """ Import python modules"""
 from flask import Flask, jsonify, request, abort
 
+
 app = Flask(__name__)
 #users = []
 
 
-comms = [{'id' : 1, 'title' : u'this is one', 'content' : u'this is the content'},
+posts = [{'id' : 1, 'title' : u'this is one', 'content' : u'this is the content'},
          {'id' : 2, 'title' : u'this is two', 'content' : u'this is the second content'}]
-users = [{'id' : 1, 'username': 'John', 'email': 'email@username', 'password': 'password',
- 'password_confirmation': 'password_confirmation'}, {'id' : 1, 'username': 'John', 'email': 'email@username',
-  'password': 'password', 'password_confirmation': 'password_confirmation'}]
-user_info ={}
+
+user_info = {}
 
 # Root/Home endpoint
 @app.route('/', methods=['GET'])
@@ -22,63 +21,68 @@ def home():
 @app.route('/level/api/v1/register', methods=['POST'])
 def register():
     """ This is a function for refistering users  """
-    if not request.json or not 'username' in request.json:
-        abort(400)
-    user = {
-    'id': users[-1]['id'] + 1,
-    'username' : request.json['username'],
-    'email' : request.json['email'],
-    'password' : request.json['password'],
-    'password_confirmation' : request.json['password_confirmation']
+    username = request.get_json()["username"]
+    email = request.get_json()["email"]
+    password = request.get_json()["password"]
+    password_confirmation = request.get_json()["password_confirmation"]
 
-    }
+    if not username or len(username.strip()) == 0:
+        return jsonify({"message": "Username cannot be blank"}), 401
+    elif not email:
+        return jsonify({"message": "Email cannot be blank"}), 401
 
-    users.append(user)
-    if not 'email' in request.json and type(request.json['email']()) != unicode:
-    	abort (400)
-    return jsonify({'user': user})
-
+    elif not password:
+        return jsonify({"message": "Password cannot be blank"}), 401
+    user_info.update({username:{"email": email, "password": password,
+                                "password_confirmation": password_confirmation}})
+    if username in user_info:
+        return jsonify({'message': "You are successfuly Registered"})
+    return jsonify({'message': "You are successfuly Registered"})
  # Login user endpoint
 @app.route('/level/api/v1/login', methods=['POST'])
 def login():
     """ This is a function for loggin a user """
-    loger = [user for user in users if user['email'] == email]
-    return jsonify({'email' : "You are logged in"})
+    username = request.get_json()["username"]
+    password = request.get_json()["password"]
+    if username in user_info:
+        if password == user_info[username]["password"]:
+            return jsonify({"message": "You are successfuly  logged in"})
+    return jsonify({'message': "You are successfuly Registered"})
 # Add comment endpoint
 @app.route('/level/api/v1/add_comment', methods=['POST'])
 def add_comment():
     """ This is a function for adding comments"""
     if not request.json or not 'title' in request.json:
-    	abort(400)
-    comm = {
-            'id' : comms[-1]['id'] + 1,
-            'title': request.json['title'],
-            'content': request.json['content'],
+        abort(400)
+    post = {
+        'id' : posts[-1]['id'] + 1,
+        'title': request.json['title'],
+        'content': request.json['content'],
     }
-    comms.append(comm)
-    return jsonify({'comm' : comm})
+    posts.append(post)
+    return jsonify({'post' : post})
 
 # List comments
 @app.route('/level/api/v1/comments', methods=['GET'])
 def comments():
     """ This is a function listing registering users"""
-    return jsonify({'comms' : comms})
+    return jsonify({'posts' : posts})
 
 # Delete comment
-@app.route('/level/api/v1/comments/<int:comm_id>', methods=['DELETE'])
-def delete_comment(comm_id):
-    comm = [comm for comm in comms if comm['id'] == comm_id]
-    if len(comm) == 0:
-         return jsonify({'message' : "No comment"})
-    comms.remove(comm[0])
+@app.route('/level/api/v1/comments/<int:post_id>', methods=['DELETE'])
+def delete_comment(post_id):
+    """ This is a function for deleting a comment """
+    post = [post for post in posts if post['id'] == post_id]
+    if len(post) == 0:
+        return jsonify({'message' : "No comment"})
+    posts.remove(post[0])
     return jsonify({'result': True})
 
  # User details endpoint
-@app.route('/level/api/v1/account/<int:user_id>', methods=['GET'])
-def user_details(user_id):
+@app.route('/level/api/v1/account', methods=['GET'])
+def user_details():
     """ This is a function fetchs user info """
-    uzer = [user for user in users if user['id'] == user_id]
-    return jsonify({'user' : uzer[0]})
+    return jsonify(user_info)
 
 
 if __name__ == '__main__':
