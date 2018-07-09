6d739e9006e77b0e2decc39f02626d8811098d2b
DennisDaddy <softteez95@gmail.com>
2018-07-09 23:52:19 +0300

    Create comments end point

--- Api/app/__init__.py
+++ Api/app/__init__.py
@@ -1,5 +1,5 @@
 """ Third party imports """
-from flask import Flask
+from flask import Flask, request, jsonify, abort
 from flask_sqlalchemy import SQLAlchemy
 
 """Local imports"""
@@ -9,10 +9,47 @@ from config import app_config
 db = SQLAlchemy()
 
 def create_app(config_name):
+    from app.models import Comment
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
+
+    @app.route('/comments/', methods=['POST', 'GET'])
+
+    def comments():
+        if request.method == "POST":
+            title = str(request.data.get('title', ''))
+            if title:
+                comment = Comment(title=title)
+                comment.save()
+                response = jsonify({
+                    'id': comment.id,
+                    'tittle': comment.tittle,
+                    'content': comment.content,
+                    'date_created': comment.date_created
+                })
+
+                response.status_code = 201
+                return response
+
+            else:
+                comments = Comment.get_all()
+                result = []
+
+                for comment in comments:
+                    obj = {
+
+                        'id': comment.id,
+                        'tittle': comment.tittle,
+                        'content': comment.content,
+                        'date_created': comment.date_created
+                    }
+
+                    results.append(obj)
+                    response = jsonify(results)
+                    response.status_code= 200
+                    return response
 
     return app
